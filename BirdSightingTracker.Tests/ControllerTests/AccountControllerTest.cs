1b72d898ab2833f56b84555b2c0662ee0cd0dca8
Jim Klein <j.mesklein@gmail.com>
2016-09-16 21:50:53 -0700

    add basic controller tests.

---
+++ BirdSightingTracker.Tests/ControllerTests/AccountControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BirdSightingTracker.Controllers;
+using BirdSightingTracker.Models;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirdSightingTracker.Tests.ControllerTests
+{
+    public class AccountControllerTest
+    {
+        //[Fact]
+        //public void Get_ViewResult_Index_Test()
+        //{
+            ////Arrange
+            //AccountController controller = new AccountController();
+
+            ////Act
+            //var result = controller.Index();
+
+            ////Assert
+            //Assert.IsType<ViewResult>(result);
+        //}
+    }
+}
---
+++ BirdSightingTracker.Tests/ControllerTests/SightingsControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BirdSightingTracker.Controllers;
+using BirdSightingTracker.Models;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirdSightingTracker.Tests.ControllerTests
+{
+    public class SightingsControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Index_Test()
+        {
+            //Arrange
+            var controller = new SightingsController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
